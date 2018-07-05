d3067300c0be0ed7e4f66b989e32406b76c0bded
Ashish Acharya <ashish.acharya14@gmail.com>
2018-07-05 19:57:18 +0545

    fixed some bugs in the position parsing

--- Board.py
+++ Board.py
@@ -1,3 +1,5 @@
+import re
+
 from Point import Point
 from enum import Enum
 
@@ -17,6 +19,11 @@ class Board(object):
         # parse the position string, if available
         if position:
             self.parse_position(position)
+        else:
+            self.tigerPos = [0, 4, 20, 24]
+
+            for i in self.tigerPos:
+                self.points[i].set_state("T")
 
     def point(self, coord):
         file = coord[0]
@@ -28,14 +35,10 @@ class Board(object):
         return self.points[(Point.File[file].value - 1) + (int(rank) - 1) * 5]
 
     def reset(self):
-        """Resets the board to its initial position"""
+        """Resets the board"""
 
         self.points = [Point(i, j) for i in range(1, 6) for j in range(1, 6)]
-        self.tigerPos = [0, 4, 20, 24]
-
-        for i in self.tigerPos:
-            self.points[i].set_state("T")
-
+        self.tigerPos = []
         self.goatsToBePlaced = 20
         self.deadGoats = 0
         self.turn = self.Player.G
@@ -54,17 +57,31 @@ class Board(object):
         print("Turn: %s" % ("Goat" if self.turn == self.Player.G else "Tiger"))
         print("Remaining Goats: %d" % self.goatsToBePlaced)
         print("Dead Goats: %d" % self.deadGoats)
+        print("Tiger Pos: %s" % str(self.tigerPos))
 
     def _get_full_position(self, pos_string):
+        """
+        get a full position from a shortened position string:
+        eg. 1GG1G/1GGGT/GGGGG/1GTGG/GTGTG gives
+        EGGEGEGGGTGGGGGEGTGGGTGTG
+        """
+
         full_pos = []
-        for i in pos_string.upper():
-            if i == 'G':
-                full_pos.append('G')
-            elif i == 'T':
-                full_pos.append('T')
-            elif i.isdigit():
-                for j in range(int(i)):
-                    full_pos.append('E')
+        pos_string = pos_string.upper().split('/')
+
+        for row in pos_string:
+            row_pos = []
+            for i in row:
+                if i == 'G':
+                    row_pos.append('G')
+                elif i == 'T':
+                    row_pos.append('T')
+                elif i.isdigit():
+                    for j in range(int(i)):
+                        row_pos.append('E')
+            # check the validity of each row and print invalid rows here
+            assert len(row_pos) % 5 == 0, "Invaild row %s. row_pos: %s" % (row, ''.join(row_pos))
+            full_pos.extend(row_pos)
         return full_pos
 
     def parse_position(self, position):
@@ -75,9 +92,17 @@ class Board(object):
         for idx, p in enumerate(full_pos):
             self.points[idx].set_state(p)
 
+        self.turn = self.Player[parts[1].upper()]
 
-b = Board("1GG1G/1GGGT/GGGGG/1GTGG/GTGTG t g0 c3 mA3")
-# print(b)
+        self.goatsToBePlaced = int(parts[2][1:])
+        assert (self.goatsToBePlaced in range(21)), "Invalid goatsToBePlaced: %d" % self.goatsToBePlaced
 
-b.point('E5').set_state("T")
-b.show()
+        self.deadGoats = int(parts[3][1:])
+        assert (self.deadGoats in range(6)), "Invalid deadGoats: %d" % self.deadGoats
+
+        self._set_tiger_positions()
+
+    def _set_tiger_positions(self):
+        for idx, p in enumerate(self.points):
+            if p.get_state() == Point.State.T:
+                self.tigerPos.append(idx)
