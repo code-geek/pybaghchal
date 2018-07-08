0b5913650227496017b1080941293cbda5b37ced
Ashish Acharya <ashish.acharya14@gmail.com>
2018-07-09 00:40:24 +0545

    some functions to play the game on the commandline

--- Game.py
+++ Game.py
@@ -1,3 +1,5 @@
+import sys
+
 from Engine import Engine
 from Board import Board
 
@@ -5,26 +7,57 @@ from Board import Board
 class Game(object):
     """Handles the operations of a Game"""
 
-    def __init__(self):
+    def __init__(self, position=None):
         super(Game, self).__init__()
-        self.board = Board()
-        self.engine = Engine(self.board, depth=12)
+        self.board = Board(position)
+        self.engine = Engine(self.board, depth=7)
 
     def input_move(self):
-        idx = int(input())
+        idx = None
+        while True:
+            try:
+                idx = int(input())
+            except:
+                print("Invalid Move")
+                continue
+            else:
+                break
+        if idx == 99:
+            sys.exit()
         return idx
 
     def human_move(self):
         moves = self.engine.generate_move_list()
         self.board.show()
         print("Please choose one move from the list.")
-        print('  |  '.join("%d. %s" % (i, str(move)) for i, move in enumerate(moves)))
+        print('\n'.join("%d. %s" % (i, str(move)) for i, move in enumerate(moves)))
 
         self.engine._make_move(moves[self.input_move()])
-        self.engine.make_best_move()
 
 
-game = Game()
+def play():
+    game = Game()
 
-while not game.board.winner:
-    game.human_move()
+    while not game.board.winner:
+        game.human_move()
+        if game.board.winner:
+            break
+        game.engine.make_best_move()
+
+    game.board.show()
+    print(game.board.winner)
+
+
+def play_random_game():
+    game = Game()
+    # move_num = 1
+    while not game.board.winner:
+        # print(move_num)
+        # move_num += 1
+        game.engine.make_best_move()
+        game.board.show()
+
+    return game.board.winner
+
+
+play()
