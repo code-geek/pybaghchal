c900a48c6437e26326248dc130a5cec658fdb0ce
Ashish Acharya <ashish.acharya14@gmail.com>
2018-07-09 17:27:14 +0545

    some tests for Board

--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
 import unittest
 
 from Point import Point
+from Board import Board
 
 
-class TestPoint(unittest.TestCase):
+class PointTestCase(unittest.TestCase):
     def setUp(self):
         self.points = [Point(i) for i in range(25)]
 
@@ -27,3 +28,38 @@ class TestPoint(unittest.TestCase):
 
     def test_str(self):
         self.assertEqual(str(self.points[0]), 'A1')
+
+
+class BoardTestCase(unittest.TestCase):
+    def setUp(self):
+        self.board = Board()
+        self.board2 = Board('1GG1G/1GGGT/GGGGG/1GTGG/GTGTG t g0 c3 mA3')
+        self.t_win = Board('G4/GG2T/GG1GG/TG1TG/TGGGG t g0 c5 mC4E4')
+        self.g_win = Board('1GG1G/1GGGT/1GGGG/GGTGG/GTGTG t g0 c3 mA3')
+
+    def test_init(self):
+        self.assertEqual(self.board.tigerPos, [0, 4, 20, 24])
+
+    def test_reset(self):
+        self.assertEqual(len(self.board.points), 25)
+        self.assertEqual(self.board.goatsToBePlaced, 20)
+        self.assertEqual(self.board.deadGoats, 0)
+        self.assertEqual(self.board.turn, Board.Player.G)
+        self.assertEqual(self.board.lastMove, "")
+
+    def test_get_full_position(self):
+        self.assertEqual(Board._get_full_position('1GG1G/1GGGT/GGGGG/1GTGG/GTGTG'), list('EGGEGEGGGTGGGGGEGTGGGTGTG'))
+        self.assertEqual(Board._get_full_position(self.t_win.position.split()[0]), list('GEEEEGGEETGGEGGTGETGTGGGG'))
+
+    def test_position(self):
+        self.assertEqual(self.board2.position, '1GG1G/1GGGT/GGGGG/1GTGG/GTGTG t g0 c3 mA3')
+        self.assertEqual(self.g_win.position, '1GG1G/1GGGT/1GGGG/GGTGG/GTGTG t g0 c3 mA3')
+
+    def test_winner(self):
+        self.assertEqual(self.t_win.winner, Board.Player.T)
+        self.assertEqual(self.g_win.winner, Board.Player.G)
+        self.assertIsNone(self.board.winner)
+        self.assertIsNone(self.board2.winner)
+
+    def test_parse_position(self):
+        pass
